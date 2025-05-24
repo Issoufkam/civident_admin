4a65bbff192039f9bd21b94220af61c0f1f9bfa5
Cedric kassi <toutoukpokassi18@gmail.com>
2025-05-24 16:30:55 +0000

    level

--- resources/views/admin/agents/index.blade.php
+++ resources/views/admin/agents/index.blade.php
@@ -109,10 +109,10 @@
                         <i class="bi bi-people-fill fs-4"></i>
                         <h1 class="h3 mb-0">Liste des agents</h1>
                     </div>
-                    <a href="#" class="btn btn-success">
+                    <a href="{{ route('admin.agents.create') }}" class="btn btn-success">
                         <i class="bi bi-plus-lg"></i>
                         <span>Ajouter un agent</span>
-                    </a>
+                   </a>
                 </div>
 
                 <div class="p-4">
